428d42af9764470482f1e436f2e69508373502a3
Dylan Wolfe <wolfed9902@gmail.com>
2020-09-03 23:18:00 -0400

    Add WTForm CharacterForm to index.html

--- mabinyogi/forms.py
+++ mabinyogi/forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import InputRequired, DataRequired, Length
 
 class CharacterForm(FlaskForm):
     char_name = StringField('Name', validators=[InputRequired()])
-    char_race = SelectField('Race', validators=[DataRequired()])
-    char_stature = SelectField('Stature', validators=[DataRequired()])
-    char_class = SelectField('Class', validators=[DataRequired()])
+    char_race = SelectField('Race', validators=[DataRequired()], choices=[('Human'),('Orc'),('Elf')])
+    char_stature = SelectField('Stature', validators=[DataRequired()], choices=[('Feminine'),('Masculine')])
+    char_class = SelectField('Class', validators=[DataRequired()], choices=[('Warrior'),('Rogue'),('Mage')])
--- mabinyogi/routes.py
+++ mabinyogi/routes.py
@@ -4,17 +4,18 @@
 - Each page will be declared by their own function as seen below
 '''
 from flask import render_template, url_for, redirect
-from mabinyogi import app, forms #importing the flask application to specify routes
+from mabinyogi import app #importing the flask application to specify routes
 from mabinyogi.cluster import mage_collection, rogue_collection, warrior_collection
+from mabinyogi.forms import CharacterForm
 
 #we define a specific web page route by using the @app.route decorator, think of it as adding a route to our application
-@app.route('/')
-@app.route('/home') #notice we can specify more than one routing name for the same route
+@app.route('/', methods=('GET','POST'))
+@app.route('/home', methods=('GET','POST')) #notice we can specify more than one routing name for the same route
 def homepage():
-    form = forms.CharacterForm()
+    form = CharacterForm()
     if form.validate_on_submit():
         return redirect(url_for('homepage'))
-    return render_template('index.html')
+    return render_template('index.html', form=form)
 
 @app.route('/about')
 def aboutpage():
